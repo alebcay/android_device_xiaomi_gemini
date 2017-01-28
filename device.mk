9af7f5d0db49a4c6e6dd72da471ea4b1ca19756b
Offain <fabian.leutenegger@bluewin.ch>
2017-01-28 02:55:47 +0100

    gemini: add libbthost_if to product_packages
    Its needed for a2dp

--- device.mk
+++ device.mk
@@ -94,6 +94,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     libaudio-resampler \
     libaudioroute \
+    libbthost_if \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
