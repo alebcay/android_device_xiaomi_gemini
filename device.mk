13c97684226d3a96dcde6507e8aa6441ed1cc726
Bruno Martins <bgcngm@gmail.com>
2017-03-29 23:22:37 +0100

    gemini: Allow control over PocketMode service
     * Once the service is running, proximity sensor is constantly active
       when the display is turned off, resulting into a residual increase
       in battery consumption. Add a toggle so that users can decide whether
       they accept that and prefer to prevent accidental wake-ups triggered
       by the fingerprint sensor.
     * Keep the receiver that listens for the screen status registered only
       if the fingerprint wake-up feature is enabled at the same time as
       the accidental wake-up prevention feature.
     * Set PocketMode as a required module of ConfigPanel, to make sure
       it is only shipped on devices building the latter.
    
    Change-Id: I5941f445d0bd77be1efa9ffcde2d9854e6446c3d

--- device.mk
+++ device.mk
@@ -162,8 +162,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    fingerprintd \
-    XiaomiPocketMode
+    fingerprintd
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
