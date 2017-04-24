ad5f2faac0a766a7d686fd60d40d58002d271e90
Offain <fabian.leutenegger@bluewin.ch>
2017-04-24 23:00:04 +0200

    gemini: add back CAF ril

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,6 +196,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # RIL
 BOARD_RIL_CLASS := ../../../$(DEVICE_PATH)/ril
 PROTOBUF_SUPPORTED := true
+TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
